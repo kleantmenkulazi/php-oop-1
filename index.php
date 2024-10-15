a320edd881990784adf2e957c74dd371047abb72
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-10-15 05:27:13 +0200

    Aggiunta funzione di stampa

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ class Movie {
 function __construct(
     string $title, 
     string $director, 
-    array $actors, 
+    array | string $actors, 
     int $year, 
     string $genres = null, 
     string $plot = null, 
@@ -49,6 +49,39 @@ public function setYear($year) {
     else {
          showError('Anno non valido');
     }
+}
+public function movieToString() {
+    $movieString = "<ul>".
+                    "<li>".
+                        "Titolo: ".$this->title.
+                    "</li>".
+                    "<li>".
+                        "Regista: ".$this->director.
+                    "</li>";
+    if (is_string($this->actors)) {
+        $movieString .= "<li>".
+            "Attori: ".$this->actors.
+        "</li>"
+    }.
+    else if (is array($this->actors)) {
+        $movieString .= "<li>".
+                        "Attori: ";
+        foreach ($this->actors as $i => $actors) {
+            $movieString .= $actor;
+
+            if ($i < count($this->actors) - 1) {
+                $movieString .= ',';    
+            }
+        }
+
+        $movieString .= "</li>";
+    }
+    
+    $movieString .= "<li>".
+                "Anno: ".$this->year.
+            "</li>".
+        "</ul>".
+    return $movieString;                
 }
 }
 
@@ -90,6 +123,15 @@ var_dump($pulpFiction->getTitle());
     <title>php oop 1</title>
 </head>
 <body>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <?php echo $pulpFiction->movieToString(); ?>
+                </div>
+            </div>
+        </div>
+    </main>
     
 </body>
 </html>
