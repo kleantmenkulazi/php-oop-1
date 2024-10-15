f69ca38f5184e5796f1e4da5b890cf6def180a50
kleantmenkulazi <kleantmenkulazi@gmail.com>
2024-10-15 03:18:18 +0200

    Creazioni classi

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+class Movie {
+    public $title;        // string
+    public $director;     // string
+    public $actors;       // string / array di string
+    public $year;         // int
+    public $genre;        // string / array di string
+    public $plot;         // string
+    public $duration;     // int
+    public $vote;        // float
+}
+$pulpFiction = new Movie();
+var_dump($pulpFiction);
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
